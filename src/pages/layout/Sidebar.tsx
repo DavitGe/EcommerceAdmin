283d54d5d62d2515f97d31b67bd07b27ab136758
Davit_Gelovani <65369123+DavitGe@users.noreply.github.com>
2023-12-10 14:44:05 +0400

    Layout

--- src/pages/layout/Sidebar.tsx
+++ src/pages/layout/Sidebar.tsx
@@ -1,7 +1,19 @@
-import { Layout, theme } from "antd";
-const { Sider: Wrapper } = Layout;
+import { DashboardOutlined, HeatMapOutlined } from "@ant-design/icons";
+import { Layout, theme, Typography, Menu } from "antd";
+import { ItemType, MenuItemType } from "antd/es/menu/hooks/useItems";
+import styled from "styled-components";
 
+const { Sider: Wrapper } = Layout;
+const { Title } = Typography;
 const { useToken } = theme;
+
+const items: ItemType<MenuItemType>[] = [
+  {
+    label: "Dashboard",
+    icon: <DashboardOutlined />,
+    key: "dashboard",
+  },
+];
 const Sidebar = () => {
   const { token } = useToken();
   return (
@@ -10,10 +22,37 @@ const Sidebar = () => {
       style={{
         minHeight: "100vh",
         backgroundColor: token.colorBgBase,
-        boxShadow: `-24px 10px 50px ${token.colorPrimary}`,
+        borderRight: `1px solid ${token.colorBorder}`,
+        boxShadow: `-22px 10px 50px ${token.colorPrimary}`,
       }}
-    />
+    >
+      <LogoContainer>
+        <HeatMapOutlined
+          style={{ fontSize: token.fontSizeXL, color: token.colorPrimary }}
+        />
+        <Title level={3} style={{ cursor: "pointer" }}>
+          Admink
+        </Title>
+      </LogoContainer>
+      <Menu items={items} />
+    </Wrapper>
   );
 };
+
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  width: 100%;
+  height: 96px;
+
+  cursor: pointer;
+
+  h3 {
+    padding: 0;
+    margin: 0;
+  }
+`;
 
 export default Sidebar;
