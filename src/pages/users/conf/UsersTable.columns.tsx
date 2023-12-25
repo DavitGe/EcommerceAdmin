0a8ed44265f48267adc02e1f9c91c5db0d82ee04
Davit_Gelovani <65369123+DavitGe@users.noreply.github.com>
2023-12-25 13:39:08 +0400

    Filters for users

--- src/pages/users/conf/UsersTable.columns.tsx
+++ src/pages/users/conf/UsersTable.columns.tsx
@@ -1,7 +1,8 @@
 import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
 import { ProColumns } from "@ant-design/pro-components";
-import { Avatar, Space, Tag, Typography } from "antd";
+import { Avatar, Col, Space, Tag, Typography } from "antd";
 import { ValueType } from "recharts/types/component/DefaultTooltipContent";
+import { filtersEnum } from "../../../components/shared/refferenceWrapper/filtersEnum";
 
 export interface UsersDataType {
   name: string;
@@ -11,9 +12,16 @@ export interface UsersDataType {
   id: number;
 }
 
+const SearchEl = filtersEnum.search;
+
 export const usersTableColumns: ProColumns<UsersDataType, ValueType>[] = [
   {
-    title: "Name",
+    title: (
+      <Col>
+        <span>Name</span>
+        <SearchEl fieldProps={{ size: "small" }} />
+      </Col>
+    ),
     dataIndex: "name",
     sorter: (a, b) => a.name.localeCompare(b.name),
     render: (_, row, index) => {
@@ -35,22 +43,42 @@ export const usersTableColumns: ProColumns<UsersDataType, ValueType>[] = [
     },
   },
   {
-    title: "Email",
+    title: (
+      <Col>
+        <span>Email</span>
+        <SearchEl fieldProps={{ size: "small" }} />
+      </Col>
+    ),
     dataIndex: "email",
     sorter: (a, b) => a.email.localeCompare(b.email),
   },
   {
-    title: "Phone",
+    title: (
+      <Col>
+        <span>Phone</span>
+        <SearchEl fieldProps={{ size: "small" }} />
+      </Col>
+    ),
     dataIndex: "phone",
     sorter: (a, b) => a.phone.localeCompare(b.phone),
   },
   {
-    title: "Address",
+    title: (
+      <Col>
+        <span>Address</span>
+        <SearchEl fieldProps={{ size: "small" }} />
+      </Col>
+    ),
     dataIndex: "address",
     sorter: (a, b) => a.address.localeCompare(b.address),
   },
   {
-    title: "Actions",
+    title: (
+      <Col>
+        <span>Actions</span>
+        <div style={{ width: "100%", height: 24 }} />
+      </Col>
+    ),
     valueType: "option",
     render: (_, record, __, action) => (
       <Space>
@@ -74,5 +102,6 @@ export const usersTableColumns: ProColumns<UsersDataType, ValueType>[] = [
         </Tag>
       </Space>
     ),
+    // width: 64,
   },
 ];
