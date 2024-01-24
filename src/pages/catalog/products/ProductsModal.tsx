f26350cbad01228a3bbd16548ff3e35e14fe9975
Davit_Gelovani <65369123+DavitGe@users.noreply.github.com>
2024-01-24 17:20:53 +0400

    products modal

--- src/pages/catalog/products/ProductsModal.tsx
+++ src/pages/catalog/products/ProductsModal.tsx
@@ -1,25 +1,122 @@
-import { ModalFormProps } from "@ant-design/pro-components";
+import {
+  ModalFormProps,
+  ProFormDigit,
+  ProFormSelect,
+  ProFormText,
+} from "@ant-design/pro-components";
 import EditModalForm from "../../../components/shared/editModal/EditModalForm";
 import { useModal } from "../../../utils/context/IsModalOpenContext";
 import { useModalData } from "./context/useModalData";
+import { Flex, Space, UploadProps, theme } from "antd";
+import Dragger from "antd/es/upload/Dragger";
+import styled from "styled-components";
+import { InboxOutlined } from "@ant-design/icons/lib/icons";
 
 interface IProps extends ModalFormProps {}
 
 const ProductsModal = (props: IProps) => {
   const { isModalOpen, setIsModalOpen } = useModal();
   const { setModalData } = useModalData();
+  const { token } = theme.useToken();
+
+  const draggerProps: UploadProps = {
+    name: "file",
+    multiple: true,
+    action: "https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188",
+    onChange(info) {
+      const { status } = info.file;
+      if (status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+      if (status === "done") {
+        // message.success(`${info.file.name} file uploaded successfully.`);
+      } else if (status === "error") {
+        // message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+    onDrop(e) {
+      console.log("Dropped files", e.dataTransfer.files);
+    },
+  };
+
   return (
     <EditModalForm
       open={isModalOpen}
       onOpenChange={setIsModalOpen}
+      title="Product add/edit"
+      width={1024}
       modalProps={{
         afterClose: () => {
           setModalData({});
         },
       }}
       {...props}
-    ></EditModalForm>
+    >
+      <Space size={32}>
+        <Dragger {...draggerProps}>
+          <p className="ant-upload-drag-icon">
+            <InboxOutlined />
+          </p>
+          <p className="ant-upload-text">
+            Click or drag file to this area to upload
+          </p>
+          <p className="ant-upload-hint">
+            Support for a single or bulk upload. Strictly prohibited from
+            uploading company data or other banned files.
+          </p>
+        </Dragger>
+        <div>
+          <ProFormText label="Name" />
+          <ProFormSelect label="Category" />
+          <Flex justify={"space-between"} gap={24} style={{ width: "100%" }}>
+            <ProFormDigit
+              label="Price"
+              placeholder={""}
+              addonAfter={
+                <PriceInputIcon color={token.colorSuccess}>
+                  <span>$</span>
+                </PriceInputIcon>
+              }
+              fieldProps={{
+                style: {
+                  paddingLeft: 24,
+                },
+              }}
+            />
+            <ProFormDigit
+              label="Discount"
+              placeholder={""}
+              addonAfter={
+                <PriceInputIcon color={token.colorError}>
+                  <span>$</span>
+                </PriceInputIcon>
+              }
+              fieldProps={{
+                style: {
+                  paddingLeft: 24,
+                },
+              }}
+            />
+          </Flex>
+          <ProFormDigit label="Count" placeholder={""} />
+        </div>
+      </Space>
+    </EditModalForm>
   );
 };
+
+const PriceInputIcon = styled.div<{ color: string }>`
+  position: absolute;
+  height: 26px;
+  width: 26px;
+  background: ${(props) => props.color};
+  top: 3px;
+  left: 3px;
+  border-radius: 4px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default ProductsModal;
